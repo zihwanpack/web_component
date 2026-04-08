21b60fa3f38c9f6a91b149ebd63815e637e8e76f
Layton <jihwan8807@naver.com>
2026-04-08 12:44:04 +0900

    필요없는 파일 삭제

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const $app = document.querySelector("#app");
 
-setupCounter(document.querySelector('#counter'))
+let state = {
+  items: ["item1", "item2", "item3", "item4"],
+};
+
+const render = () => {
+  const { items } = state;
+  $app.innerHTML = `
+    <ul>
+      ${items.map((item) => `<li>${item}</li>`).join("")}
+    </ul>
+    <button id="append">추가</button>
+  `;
+  document.querySelector("#append").addEventListener("click", () => {
+    setState({ items: [...items, `item${items.length + 1}`] });
+  });
+};
+
+const setState = (newState) => {
+  state = { ...state, ...newState };
+  render();
+};
+
+render();
